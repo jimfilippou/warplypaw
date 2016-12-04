5d5e991d6aa1ffe633175985e419d0485fdd44ed
Jim <jimfilippou@hotmail.gr>
2016-12-05 01:39:16 +0200

    Update WarplyPaw.py

--- WarplyPaw.py
+++ WarplyPaw.py
@@ -88,10 +88,6 @@ while True:
                         elif message == "new_week_inform":
                             send_message("Week " + str(datetime.date.today().isocalendar()[1]) + " Has Started! Get Your Timesheets Started ",user)
                         
-                        elif message == "get template":
-
-                            send_message("Test",user)
-
                         else:
                             continue
                     else:
